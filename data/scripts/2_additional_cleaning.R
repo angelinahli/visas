a66f7d524bbae7da8680ead0f652b4708a0e6d95
Angelina Li <ali6@wellesley.edu>
2020-01-02 17:45:21 -0500

    modified file structure a little

---
+++ data/scripts/2_additional_cleaning.R
@@ -0,0 +1,20 @@
+## Filename:    2_additional_cleaning.R
+## Author:      Angelina Li
+## Date:        2020/01/02
+## R version:   3.3.3
+## Description: Take some aditional cleaning measures in order to have data in the format I'd like
+
+# Clear old variables
+rm(list = ls())
+
+###### Load in Packages ######
+
+library(  )
+
+###### Defining Paths ######
+
+base_path   <- file.path()
+input_path  <- file.path(base_path, "Input")
+output_path <- file.path(base_path, "Output")
+
+###### Load in Data ######
